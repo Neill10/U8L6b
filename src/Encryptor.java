a1e4b637ec2a3536663f63702619fb6375985542
BT_1E10_33 <sample@bths.edu>
2022-03-10 12:23:35 -0500

    finished all encrypt methods

--- src/Encryptor.java
+++ src/Encryptor.java
@@ -87,16 +87,130 @@ public class Encryptor {
         int length = message.length();
         String end = "";
         int letters = numCols * numRows;
-        
-        for (int i = 0; i < (length / letters); i++) {
-            String str = message.substring(0, letters);
-            message = message.substring(letters);
-            fillBlock(str);//str should be changed by the void fillBlock method
+        if((double)length % letters == 0)//had to add this im guessing cause of perfect fitting?
+        {
+            for (int i = 0; i < (length / letters); i++) {
+                String str = message.substring(0, letters);
+                message = message.substring(letters);
+                fillBlock(str);//str should be changed by the void fillBlock method
+                end += encryptBlock();
+            }
+        }
+        else{
+            for (int i = 0; i < (length / letters); i++) {
+                String str = message.substring(0, letters);
+                message = message.substring(letters);
+                fillBlock(str);//str should be changed by the void fillBlock method
+                end += encryptBlock();
+            }
+            fillBlock(message);//str should be changed by the void fillBlock method
             end += encryptBlock();
         }
-        fillBlock(message);
+        /*
+        for (int i = 0; i < (length / letters); i++) {
+        String str = message.substring(0, letters);
+        message = message.substring(letters);
+        fillBlock(str);//str should be changed by the void fillBlock method
         end += encryptBlock();
-
+        }
+         */
         return end;
+    }
+
+    /**  Decrypts an encrypted message. All filler 'A's that may have been
+     *   added during encryption will be removed, so this assumes that the
+     *   original message (BEFORE it was encrypted) did NOT end in a capital A!
+     *
+     *   NOTE! When you are decrypting an encrypted message,
+     *         be sure that you have initialized your Encryptor object
+     *         with the same row/column used to encrypted the message! (i.e.
+     *         the “encryption key” that is necessary for successful decryption)
+     *         This is outlined in the precondition below.
+     *
+     *   Precondition: the Encryptor object being used for decryption has been
+     *                 initialized with the same number of rows and columns
+     *                 as was used for the Encryptor object used for encryption.
+     *
+     *   @param encryptedMessage  the encrypted message to decrypt
+     *
+     *   @return  the decrypted, original message (which had been encrypted)
+     *
+     *   TIP: You are encouraged to create other helper methods as you see fit
+     *        (e.g. a method to decrypt each section of the decrypted message,
+     *         similar to how encryptBlock was used)
+     */
+    public String decryptMessage(String encryptedMessage)
+    {
+        String[][] string = new String[numRows][numCols];
+        int length = encryptedMessage.length();
+        String end = "";
+        int letters = numCols * numRows;
+        if((double)length % letters == 0)//had to add this im guessing cause of perfect fitting?
+        {
+            for (int i = 0; i < (length / letters); i++) {
+                String str = encryptedMessage.substring(0, letters);
+                encryptedMessage = encryptedMessage.substring(letters);
+                fillBlock2(str);//str should be changed by the void fillBlock method
+                end += printContent(getLetterBlock());
+            }
+        }
+        else{
+            for (int i = 0; i < (length / letters); i++) {
+                String str = encryptedMessage.substring(0, letters);
+                encryptedMessage = encryptedMessage.substring(letters);
+                fillBlock2(str);//str should be changed by the void fillBlock method
+                end += printContent(getLetterBlock());
+            }
+            fillBlock(encryptedMessage);//str should be changed by the void fillBlock method
+            end += printContent(getLetterBlock());;
+        }
+        int endLength = end.length();
+        int indexCount = endLength - 1;
+        for(int a = end.length(); a > 0 ; a--)
+        {
+            if(end.substring(indexCount).equals("A"))
+            {
+                end = end.substring(0,indexCount);
+            }
+            else
+            {
+                break;
+            }
+            indexCount--;
+        }
+        return end;
+    }
+    private void fillBlock2(String str) {
+        int index = 0;
+        int length = str.length();
+        for (int a = 0; a < numCols; a++) {
+            for (int b = 0; b < numRows; b++) {
+                if (length < numCols * numRows) {
+                    if (index < length) {
+                        String string = str.charAt(index) + "";
+                        letterBlock[b][a] = string;
+                        index++;
+                    } else {
+                        letterBlock[b][a] = "A";
+                    }
+                } else {
+                    String string = str.charAt(index) + "";
+                    letterBlock[b][a] = string;
+                    index++;
+                }
+            }
+        }
+    }
+    private String printContent(String[][] array2D)
+    {
+        String str = "";
+        for(int i = 0; i < array2D.length;i++)
+        {
+            for(int x = 0 ; x < array2D[0].length;x++)
+            {
+                str += array2D[i][x];
+            }
+        }
+        return str;
     }
 }
